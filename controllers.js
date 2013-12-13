4006135c82859401408d54d504ea397a56ff0768
Marco <marco@marcoangeli.net>
2013-12-13 15:56:42 +0100

    v2

--- controllers.js
+++ controllers.js
@@ -2,7 +2,7 @@ var app = angular.module("MyApp", []);
 
 app.controller("PostsCtrl", function($scope, $http) {
   $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-  $http.get('http://www.labilingue.it/api/get_recent_posts/').
+  $http.get('posts.json').
     success(function(data, status, headers, config) {
       $scope.posts = data;
     });
